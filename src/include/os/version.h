d6c3764458819816870391f12923c7a6d8622c60
mri <mri>
2003-02-06 19:25:48 +0000

    *** empty log message ***

--- src/include/os/version.h
+++ src/include/os/version.h
@@ -1,4 +1,4 @@
 #define OSNAME       "sanos"
-#define OSVERSION    "1.2.5"
-#define RELEASE_DATE "Jan 12 2003"
+#define OSVERSION    "1.2.6"
+#define RELEASE_DATE "Feb 06 2003"
 #define COPYRIGHT    "Copyright (c) 2001-2002 Michael Ringgaard. All rights reserved."
