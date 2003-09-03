8a1c3cebcbf8bf64fb8bb63cb97f9c7ba30ef4db
mri <mri>
2003-09-03 11:32:37 +0000

    *** empty log message ***

---
+++ build.cmd
@@ -0,0 +1,5 @@
+devenv /build release /project dfs utils\dfs\dfs.sln
+devenv /build release /project mkfloppy utils\mkfloppy\mkfloppy.sln
+devenv /build release /project mkpart utils\mkpart\mkpart.sln
+devenv /build release /project dbggw utils\dbggw\dbggw.sln
+devenv /build sanos /project image build\sanos.sln
