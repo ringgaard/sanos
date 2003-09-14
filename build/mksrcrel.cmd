d88d3768e17a922483175724348562e57a55d5ac
mri <mri>
2003-09-14 20:17:56 +0000

    *** empty log message ***

--- build/mksrcrel.cmd
+++ build/mksrcrel.cmd
@@ -1,14 +1,18 @@
 if not exist %1 mkdir %1
 if not exist %1\build mkdir %1\build
 if not exist %1\src mkdir %1\src
+if not exist %1\vcwizard mkdir %1\vcwizard
 if not exist %1\tools mkdir %1\tools
 if not exist %1\utils mkdir %1\utils
 if not exist %1\utils\dfs mkdir %1\utils\dfs
 if not exist %1\utils\dbggw mkdir %1\utils\dbggw
 if not exist %1\utils\mkpart mkdir %1\utils\mkpart
+if not exist %1\utils\mkfloppy mkdir %1\utils\mkfloppy
 
 copy README %1
 copy COPYING %1
+copy FILES %1
+copy build.cmd %1
 
 copy build\*.vcproj %1\build
 copy build\sanos.sln %1\build
@@ -18,6 +22,7 @@ copy build\osrel.ini %1\build\os.ini
 copy build\setuprel.ini %1\build\setup.ini
 
 xcopy src %1\src /s /exclude:build\exclrel.lst
+xcopy vcwizard %1\vcwizard /s /exclude:build\exclrel.lst
 
 copy tools\mkdfs.exe %1\tools
 copy tools\dbggw.exe %1\tools
@@ -27,3 +32,4 @@ copy tools\nasmw.exe %1\tools
 xcopy utils\dfs %1\utils\dfs /exclude:build\exclrel.lst
 xcopy utils\dbggw %1\utils\dbggw /exclude:build\exclrel.lst
 xcopy utils\mkpart %1\utils\mkpart /exclude:build\exclrel.lst
+xcopy utils\mkfloppy %1\utils\mkfloppy /exclude:build\exclrel.lst
