1a512f974ab9c4a32e72a479d14cd9dade1c74a2
mri <mri>
2007-02-17 06:04:01 +0000

    *** empty log message ***

--- src/lib/chkstk.asm
+++ src/lib/chkstk.asm
@@ -52,10 +52,39 @@ lastpage:
 		mov     eax,dword ptr [eax + 4] ; recover return address
 
 		push    eax                     ; prepare return address
-			                        ; ...probe in case a page was crossed
+										; ...probe in case a page was crossed
 		ret
 
 __chkstk        endp
+
+public  _alloca_probe_8
+
+_alloca_probe_16 proc                   ; 16 byte aligned alloca
+
+        push    ecx
+        lea     ecx, [esp] + 8          ; TOS before entering this function
+        sub     ecx, eax                ; New TOS
+        and     ecx, (16 - 1)           ; Distance from 16 bit align (align down)
+        add     eax, ecx                ; Increase allocation size
+        sbb     ecx, ecx                ; ecx = 0xFFFFFFFF if size wrapped around
+        or      eax, ecx                ; cap allocation size on wraparound
+        pop     ecx                     ; Restore ecx
+        jmp     __chkstk
+
+alloca_8:                               ; 8 byte aligned alloca
+_alloca_probe_8 = alloca_8
+
+        push    ecx
+        lea     ecx, [esp] + 8          ; TOS before entering this function
+        sub     ecx, eax                ; New TOS
+        and     ecx, (8 - 1)            ; Distance from 8 bit align (align down)
+        add     eax, ecx                ; Increase allocation Size
+        sbb     ecx, ecx                ; ecx = 0xFFFFFFFF if size wrapped around
+        or      eax, ecx                ; cap allocation size on wraparound
+        pop     ecx                     ; Restore ecx
+        jmp     __chkstk
+
+_alloca_probe_16 endp
 
 _TEXT           ends
                 end
