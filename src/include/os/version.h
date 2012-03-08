9cdc03c7cfbbd3094c9ef3fc7feb4c8a701eb1a6
mri <mri>
2012-03-08 15:00:56 +0000

    *** empty log message ***

--- src/include/os/version.h
+++ src/include/os/version.h
@@ -1,8 +1,8 @@
 #define VERSION_H
 #define OS_NAME      "Sanos"
-#define OS_COPYRIGHT "Copyright (c) 2001-2005 Michael Ringgaard."
+#define OS_COPYRIGHT "Copyright (c) 2001-2012 Michael Ringgaard."
 #define OS_LEGAL     "All rights reserved."
 #define OS_MAJ_VERS  1
 #define OS_MIN_VERS  3
-#define OS_RELEASE   4
+#define OS_RELEASE   5
 #define OS_BUILD     0
