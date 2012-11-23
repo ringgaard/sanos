9fdaa4ce39480f6a835bbe28a5e7bbb145687145
Michael Ringgaard <mri@jbox.dk>
2012-11-24 00:20:44 +0100

    Convert tabs to spaces in getopt.c

--- src/lib/getopt.c
+++ src/lib/getopt.c
@@ -115,119 +115,119 @@ int getopt(int argc, char **argv, char *opts) {
 }
 
 static int getopt_internal(struct opt *opt, int argc, char **argv, const char *opts) {
-	char *oli; // option letter list index
+  char *oli; // option letter list index
 
-	if (opt->reset || !*opt->place) {
-		// update scanning pointer
-		opt->reset = 0;
-		if (opt->ind >= argc || *(opt->place = argv[opt->ind]) != '-') {
-			opt->place = "";
-			return -1;
-		}
-		if (opt->place[1] && *++opt->place == '-') {	
-		  // found "--"
-			opt->place = "";
-			return -2;
-		}
-	}
-	
-	// option letter okay?
-	if ((opt->opt = *opt->place++) == ':' || !(oli = strchr(opts, opt->opt))) {
-		 // if the user didn't specify '-' as an option, assume it means -1
-		if (opt->opt == '-') return -1;
-		if (!*opt->place) ++opt->ind;
-		if (opt->err && *opts != ':') {
-			fprintf(stderr, "%s: illegal option -- %c\n", argv[0], opt->opt);
-		}
-		return '?';
-	}
-	
-	if (*++oli != ':') {
-	  // don't need argument
-		opt->arg = NULL;
-		if (!*opt->place) ++opt->ind;
-	} else {				
-	  // need an argument
-		if (*opt->place)  {
-		  // no white space
-			opt->arg = opt->place;
-		} else if (argc <= ++opt->ind) {
-			// no arg
-			opt->place = "";
-			if (opt->err && *opts != ':') {
-				fprintf(stderr, "%s: option requires an argument -- %c\n", argv[0], opt->opt);
-		  }
-			return ':';
-		} else {
-		  // white space
-			opt->arg = argv[opt->ind];
-	  }
-		opt->place = "";
-		++opt->ind;
-	}
-	
+  if (opt->reset || !*opt->place) {
+    // update scanning pointer
+    opt->reset = 0;
+    if (opt->ind >= argc || *(opt->place = argv[opt->ind]) != '-') {
+      opt->place = "";
+      return -1;
+    }
+    if (opt->place[1] && *++opt->place == '-') {  
+      // found "--"
+      opt->place = "";
+      return -2;
+    }
+  }
+  
+  // option letter okay?
+  if ((opt->opt = *opt->place++) == ':' || !(oli = strchr(opts, opt->opt))) {
+     // if the user didn't specify '-' as an option, assume it means -1
+    if (opt->opt == '-') return -1;
+    if (!*opt->place) ++opt->ind;
+    if (opt->err && *opts != ':') {
+      fprintf(stderr, "%s: illegal option -- %c\n", argv[0], opt->opt);
+    }
+    return '?';
+  }
+  
+  if (*++oli != ':') {
+    // don't need argument
+    opt->arg = NULL;
+    if (!*opt->place) ++opt->ind;
+  } else {        
+    // need an argument
+    if (*opt->place)  {
+      // no white space
+      opt->arg = opt->place;
+    } else if (argc <= ++opt->ind) {
+      // no arg
+      opt->place = "";
+      if (opt->err && *opts != ':') {
+        fprintf(stderr, "%s: option requires an argument -- %c\n", argv[0], opt->opt);
+      }
+      return ':';
+    } else {
+      // white space
+      opt->arg = argv[opt->ind];
+    }
+    opt->place = "";
+    ++opt->ind;
+  }
+  
   // dump back option letter
-	return opt->opt;
+  return opt->opt;
 }
 
 int getopt_long(int argc, char **argv, const char *opts, const struct option *longopts, int *index) {
-	int retval;
+  int retval;
   struct opt *opt = getoptvars();
 
-	if ((retval = getopt_internal(opt, argc, argv, opts)) == -2) {
-		char *current_argv = argv[opt->ind++] + 2, *has_equal;
-		int i, current_argv_len, match = -1;
+  if ((retval = getopt_internal(opt, argc, argv, opts)) == -2) {
+    char *current_argv = argv[opt->ind++] + 2, *has_equal;
+    int i, current_argv_len, match = -1;
 
-		if (*current_argv == '\0') return -1;
+    if (*current_argv == '\0') return -1;
 
-		if ((has_equal = strchr(current_argv, '=')) != NULL) {
-			current_argv_len = has_equal - current_argv;
-			has_equal++;
-		} else {
-			current_argv_len = strlen(current_argv);
+    if ((has_equal = strchr(current_argv, '=')) != NULL) {
+      current_argv_len = has_equal - current_argv;
+      has_equal++;
+    } else {
+      current_argv_len = strlen(current_argv);
     }
 
-		for (i = 0; longopts[i].name; i++) { 
-			if (strncmp(current_argv, longopts[i].name, current_argv_len)) continue;
+    for (i = 0; longopts[i].name; i++) { 
+      if (strncmp(current_argv, longopts[i].name, current_argv_len)) continue;
 
-			if (strlen(longopts[i].name) == current_argv_len) { 
-				match = i;
-				break;
-			}
-			if (match == -1) match = i;
-		}
-		if (match != -1) {
-			if (longopts[match].has_arg == required_argument ||
-			    longopts[match].has_arg == optional_argument) {
-				if (has_equal) {
-					optarg = has_equal;
-				} else {
-					optarg = argv[opt->ind++];
-			  }
-			}
-			if (longopts[match].has_arg == required_argument && optarg == NULL) {
-				// Missing argument, leading: indicates no error should be generated
-				if (opt->err && *opts != ':') {
-					fprintf(stderr, "%s: option requires an argument -- %s\n", argv[0], current_argv);
-			  }
-				return ':';
-			}
-		} else { 
-		  // No matching argument
-			if (opt->err && *opts != ':') {
-				fprintf(stderr, "%s: illegal option -- %s\n", argv[0], current_argv);
-		  }
-			return '?';
-		}
-		if (longopts[match].flag) {
-			*longopts[match].flag = longopts[match].val;
-			retval = 0;
-		} else {
-			retval = longopts[match].val;
-	  }
-		if (index) *index = match;
-	}
-	
-	return retval;
+      if (strlen(longopts[i].name) == current_argv_len) { 
+        match = i;
+        break;
+      }
+      if (match == -1) match = i;
+    }
+    if (match != -1) {
+      if (longopts[match].has_arg == required_argument ||
+          longopts[match].has_arg == optional_argument) {
+        if (has_equal) {
+          optarg = has_equal;
+        } else {
+          optarg = argv[opt->ind++];
+        }
+      }
+      if (longopts[match].has_arg == required_argument && optarg == NULL) {
+        // Missing argument, leading: indicates no error should be generated
+        if (opt->err && *opts != ':') {
+          fprintf(stderr, "%s: option requires an argument -- %s\n", argv[0], current_argv);
+        }
+        return ':';
+      }
+    } else { 
+      // No matching argument
+      if (opt->err && *opts != ':') {
+        fprintf(stderr, "%s: illegal option -- %s\n", argv[0], current_argv);
+      }
+      return '?';
+    }
+    if (longopts[match].flag) {
+      *longopts[match].flag = longopts[match].val;
+      retval = 0;
+    } else {
+      retval = longopts[match].val;
+    }
+    if (index) *index = match;
+  }
+  
+  return retval;
 }
 
