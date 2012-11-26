a5a1e51b28e72f8f9f2d1b5ff1f4f1e0ecf5a990
Michael Ringgaard <mri@jbox.dk>
2012-11-26 22:04:50 +0100

    Added find command for text editor

--- src/utils/edit/edit.c
+++ src/utils/edit/edit.c
@@ -31,9 +31,23 @@
 // SUCH DAMAGE.
 // 
 
-#include <os.h>
+#include <signal.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#ifdef SANOS
+#include <os.h>
+#endif
+
+#ifdef __linux__
+#include <sys/ioctl.h>
+#include <termios.h>
+#define O_BINARY 0
+#endif
 
 #define MINEXTEND      32768
 #define LINEBUF_EXTRA  32
@@ -76,7 +90,7 @@
 
 #define KEY_UNKNOWN     0xFFF
 
-#define CTRL(c) ((c) - 0x60)
+#define ctrl(c) ((c) - 0x60)
 
 //
 // Editor data block
@@ -109,6 +123,7 @@ struct editor {
   int anchor;               // Anchor position for selection
   unsigned char *clipboard; // Clipboard
   int clipsize;             // Clipboard size
+  unsigned char *search;    // Search text.
 
   int refresh;              // Flag to trigger screen redraw
   int lineupdate;           // Flag to trigger redraw of current line
@@ -117,10 +132,11 @@ struct editor {
   int minihelp;             // Flag to control mini help in status line
   int newfile;              // File is a new file
 
-  int cols;
-  int lines;
-  char *linebuf;
-  char filename[MAXPATH];
+  int cols;                 // Console columns
+  int lines;                // Console lines
+
+  unsigned char *linebuf;
+  char filename[FILENAME_MAX];
 };
 
 //
@@ -182,7 +198,7 @@ int save_file(struct editor *ed, char *filename) {
 
   if (!filename) filename = ed->filename;
 
-  f = open(filename, O_CREAT | O_TRUNC, 0644);
+  f = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0644);
   if (f < 0) return -1;
 
   if (write(f, ed->start, ed->gap - ed->start) != ed->gap - ed->start) goto err;
@@ -201,6 +217,11 @@ void delete_editor(struct editor *ed) {
   if (ed->start) free(ed->start);
   if (ed->linebuf) free(ed->linebuf);
   if (ed->clipboard) free(ed->clipboard);
+  if (ed->search) free(ed->search);
+}
+
+int text_length(struct editor *ed) {
+  return (ed->gap - ed->start) + (ed->end - ed->rest);
 }
 
 void move_gap(struct editor *ed, int pos, int minsize) {
@@ -253,6 +274,12 @@ void move_gap(struct editor *ed, int pos, int minsize) {
 #ifdef DEBUG
   memset(ed->gap, 0, ed->rest - ed->gap);
 #endif
+}
+
+void close_gap(struct editor *ed) {
+  int len = text_length(ed);
+  move_gap(ed, len, 1);
+  ed->start[len] = 0;
 }
 
 int copy(struct editor *ed, unsigned char *buf, int pos, int len) {
@@ -312,10 +339,6 @@ __inline int get(struct editor *ed, int pos) {
 // Navigation functions
 //
 
-int text_length(struct editor *ed) {
-  return (ed->gap - ed->start) + (ed->end - ed->rest);
-}
-
 int line_length(struct editor *ed, int linepos) {
   int pos = linepos;
   while (1) {
@@ -362,28 +385,32 @@ int prev_line(struct editor *ed, int pos) {
   return 0;
 }
 
-void moveto(struct editor *ed, int pos) {
+void moveto(struct editor *ed, int pos, int center) {
+  int scroll = 0;
   for (;;) {
     int cur = ed->linepos + ed->col;
     if (pos < cur) {
       if (pos >= ed->linepos) {
         ed->col = pos - ed->linepos;
       } else {
+        ed->col = 0;
         ed->linepos = prev_line(ed, ed->linepos);
         ed->line--;
+
         if (ed->topline > ed->line) {
           ed->toppos = ed->linepos;
           ed->topline--;
           ed->refresh = 1;
+          scroll = 1;
         }
       }
     } else if (pos > cur) {
       int next = next_line(ed, ed->linepos);
-      if (next == -1 && pos == text_length(ed)) {
-        ed->col = pos - ed->linepos;
-      } else if (pos < next) {
+      if (next == -1) pos = text_length(ed);
+      if (pos < next) {
         ed->col = pos - ed->linepos;
       } else {
+        ed->col = 0;
         ed->linepos = next;
         ed->line++;
 
@@ -391,10 +418,27 @@ void moveto(struct editor *ed, int pos) {
           ed->toppos = next_line(ed, ed->toppos);
           ed->topline++;
           ed->refresh = 1;
+          scroll = 1;
         }
       }
     } else {
       break;
+    }
+  }
+  
+  if (scroll && center) {
+    int tl = ed->line - ed->lines / 2;
+    if (tl < 0) tl = 0;
+    for (;;) {
+      if (ed->topline > tl) {
+        ed->toppos = prev_line(ed, ed->toppos);
+        ed->topline--;
+      } else if (ed->topline < tl) {
+        ed->toppos = next_line(ed, ed->toppos);
+        ed->topline++;
+      } else {
+        break;
+      }
     }
   }
 }
@@ -403,7 +447,7 @@ void moveto(struct editor *ed, int pos) {
 // Text selection
 //
 
-void get_selection(struct editor *ed, int *start, int *end)  {
+int get_selection(struct editor *ed, int *start, int *end) {
   if (ed->anchor == -1) {
     *start = *end = -1;
     return 0;
@@ -437,7 +481,7 @@ int erase_selection(struct editor *ed) {
   int selstart, selend;
   
   if (!get_selection(ed, &selstart, &selend)) return 0;
-  moveto(ed, selstart);
+  moveto(ed, selstart, 0);
   erase(ed, selstart, selend - selstart);
   ed->anchor = -1;
   ed->refresh = 1;
@@ -447,7 +491,48 @@ int erase_selection(struct editor *ed) {
 void select_all(struct editor *ed) {
   ed->anchor = 0;
   ed->refresh = 1;
-  moveto(ed, text_length(ed));
+  moveto(ed, text_length(ed), 0);
+}
+
+//
+// Screen functions
+//
+
+void get_console_size(struct editor *ed) {
+#ifdef SANOS
+  struct term *term = gettib()->proc->term;
+  ed->cols = term->cols;
+  ed->lines = term->lines - 1;
+#else
+  struct winsize ws;
+
+  ioctl(0, TIOCGWINSZ, &ws);
+  ed->cols = ws.ws_col;
+  ed->lines = ws.ws_row - 1;
+#endif
+}
+
+void outch(char c) {
+  putchar(c);
+}
+
+void outbuf(unsigned char *buf, int len) {
+  fwrite(buf, 1, len, stdout);
+}
+
+void outstr(char *str) {
+  fputs(str, stdout);
+}
+
+void clear_screen() {
+  outstr("\033[0J");
+}
+
+void gotoxy(int col, int line) {
+  char buf[32];
+
+  sprintf(buf, "\033[%d;%dH", line + 1, col + 1);
+  outstr(buf);
 }
 
 //
@@ -468,11 +553,21 @@ int getkey() {
     case 0x0A: return gettib()->proc->term->type != TERM_CONSOLE ? KEY_ENTER : KEY_UNKNOWN;
 #else
     case 0x0D: return KEY_ENTER;
+    case 0x0A: return KEY_ENTER;
 #endif
     case 0x1B:
       ch = getchar();
       switch (ch) {
         case 0x1B: return KEY_ESC;
+        case 0x4F:
+          ch = getchar();
+          switch (ch) {
+            case 0x46: return KEY_END;
+            case 0x48: return KEY_HOME;
+            default: return KEY_UNKNOWN;
+          }
+          break;
+          
         case 0x5B:
           shift = ctrl = 0;
           ch = getchar();
@@ -543,31 +638,44 @@ int getkey() {
   }
 }
 
-//
-// Screen functions
-//
+int prompt(struct editor *ed, char *msg) {
+  int maxlen, len, ch, selstart, selend;
+  char *buf = ed->linebuf;
 
-void outch(char c) {
-  putchar(c);
-}
-
-void outbuf(char *buf, int len) {
-  fwrite(buf, 1, len, stdout);
-}
-
-void outstr(char *str) {
-  fputs(str, stdout);
-}
-
-void clear_screen() {
-  outstr("\033[0J");
-}
-
-void gotoxy(int col, int line) {
-  char buf[32];
-
-  sprintf(buf, "\033[%d;%dH", line + 1, col + 1);
-  outstr(buf);
+  gotoxy(0, ed->lines);
+  outstr("\033[1m");
+  outstr(msg);
+  outstr("\033[K\033[0m");
+  
+  len = 0;
+  if (get_selection(ed, &selstart, &selend)) {
+    int sellen = selend - selstart;
+    if (sellen < maxlen) {
+      copy(ed, buf, selstart, sellen);
+      while (len < sellen && buf[len] >= ' ') len++;
+      outbuf(buf, len);
+    }
+  }
+  
+  maxlen = ed->cols - strlen(msg) - 1;
+  for (;;) {
+    fflush(stdout);
+    ch = getkey();
+    if (ch == KEY_ESC) {
+      return 0;
+    } else if (ch == KEY_ENTER) {
+      buf[len] = 0;
+      return 1;
+    } else if (ch == KEY_BACKSPACE) {
+      if (len > 0) {
+        outstr("\b \b");
+        len--;
+      }
+    } else if (ch >= ' ' && ch < 0x100 && len < maxlen) {
+      outch(ch);
+      buf[len++] = ch;
+    }
+  }
 }
 
 //
@@ -583,24 +691,21 @@ void display_message(struct editor *ed, char *msg) {
 }
 
 void draw_full_statusline(struct editor *ed) {
-  char buf[128];
-
+  int namewidth = ed->cols - 36;
   gotoxy(0, ed->lines);
   if (ed->minihelp) {
-    sprintf(buf, "\033[1m%-40.40s Ctrl-S=Save Ctrl-Q=Quit F1=Help %s\033[K\033[0m", ed->filename, ed->newfile ? "(NEW)" : "");
+    sprintf(ed->linebuf, "\033[1m%*.*s Ctrl-S=Save Ctrl-Q=Quit F1=Help %s\033[K\033[0m", -namewidth, namewidth, ed->filename, ed->newfile ? "(NEW)" : "");
     ed->minihelp = 0;
   } else {
-    sprintf(buf, "\033[1m%-40.40s%c Ln %-6d Col %-4d Pos %-10d\033[K\033[0m", ed->filename, ed->dirty ? '*' : ' ', ed->line + 1, ed->col + 1, ed->linepos + ed->col + 1);
+    sprintf(ed->linebuf, "\033[1m%*.*s%c Ln %-6d Col %-4d Pos %-10d\033[K\033[0m", -namewidth, namewidth, ed->filename, ed->dirty ? '*' : ' ', ed->line + 1, ed->col + 1, ed->linepos + ed->col + 1);
   }
-  outstr(buf);
+  outstr(ed->linebuf);
 }
 
 void draw_statusline(struct editor *ed) {
-  char buf[128];
-
-  gotoxy(40, ed->lines);
-  sprintf(buf, "\033[1m%c Ln %-6d Col %-4d Pos %-10d\033[K\033[0m", ed->dirty ? '*' : ' ', ed->line + 1, ed->col + 1, ed->linepos + ed->col + 1);
-  outstr(buf);
+  gotoxy(ed->cols - 36, ed->lines);
+  sprintf(ed->linebuf, "\033[1m%c Ln %-6d Col %-4d Pos %-10d\033[K\033[0m", ed->dirty ? '*' : ' ', ed->line + 1, ed->col + 1, ed->linepos + ed->col + 1);
+  outstr(ed->linebuf);
 }
 
 void display_line(struct editor *ed, int pos, int margin, int len, int fullline) {
@@ -965,7 +1070,7 @@ void cut_selection(struct editor *ed) {
 void paste_selection(struct editor *ed) {
   erase_selection(ed);
   insert(ed, ed->linepos + ed->col, ed->clipboard, ed->clipsize);
-  moveto(ed, ed->linepos + ed->col + ed->clipsize);
+  moveto(ed, ed->linepos + ed->col + ed->clipsize, 0);
   ed->refresh = 1;
 }
 
@@ -986,6 +1091,35 @@ void save(struct editor *ed) {
     sleep(5);
   }
 
+  ed->refresh = 1;
+}
+
+void find(struct editor *ed, int next) {
+  int slen;
+
+  if (!next) {
+    if (!prompt(ed, "Find: ")) {
+      ed->refresh = 1;
+      return;
+    }    
+    if (ed->search) free(ed->search);
+    ed->search = strdup(ed->linebuf);
+  }
+
+  slen = strlen(ed->search);
+  if (slen > 0) {
+    unsigned char *match;
+    
+    close_gap(ed);
+    match = strstr(ed->start + ed->linepos + ed->col, ed->search);
+    if (match != NULL) {
+      int pos = match - ed->start;
+      ed->anchor = pos;
+      moveto(ed, pos + slen, 1);
+    } else {
+      outch('\007');
+    }
+  }
   ed->refresh = 1;
 }
 
@@ -1035,10 +1169,7 @@ void detab(struct editor *ed) {
 }
 
 void redraw_screen(struct editor *ed) {
-  struct term *term = gettib()->proc->term;
-  ed->cols = term->cols;
-  ed->lines = term->lines - 1;
-  ed->linebuf = realloc(ed->linebuf, ed->cols + LINEBUF_EXTRA);
+  get_console_size(ed);
   draw_screen(ed);
 }
 
@@ -1054,8 +1185,8 @@ void help(struct editor *ed) {
   outstr("<pgdn>      Move one page down (*)       Ctrl+C  Copy selection to clipboard\r\n");
   outstr("<home>      Move to start of line        Ctrl+X  Cut selection to clipboard\r\n");
   outstr("<end>       Move to end of line          Ctrl+V  Paste selection from clipboard\r\n");
-  outstr("Ctrl+<home> Move to start of file\r\n");
-  outstr("Ctrl+<end>  Move to end of file\r\n");
+  outstr("Ctrl+<home> Move to start of file        Ctrl+F  Find text\r\n");
+  outstr("Ctrl+<end>  Move to end of file          Ctrl+N  Find next\r\n");
   outstr("<backspace> Delete previous character\r\n");
   outstr("<delete>    Delete current character\r\n");
   outstr("\r\n(*) Extends selection if combined with Shift\r\n");
@@ -1119,13 +1250,16 @@ void edit(struct editor *ed) {
         case KEY_ENTER: newline(ed); break;
         case KEY_BACKSPACE: backspace(ed); break;
         case KEY_DEL: del(ed); break;
-        case CTRL('a'): select_all(ed); break;
-        case CTRL('c'): copy_selection(ed); break;
-        case CTRL('x'): cut_selection(ed); break;
-        case CTRL('v'): paste_selection(ed); break;
-        case CTRL('s'): save(ed); break;
-        case CTRL('q'): if (quit(ed)) done = 1; break;
-        case CTRL('l'): redraw_screen(ed); break;
+        case ctrl('a'): select_all(ed); break;
+        case ctrl('c'): copy_selection(ed); break;
+        case ctrl('f'): find(ed, 0); break;
+        case ctrl('g'): find(ed, 0); break;
+        case ctrl('n'): find(ed, 1); break;
+        case ctrl('x'): cut_selection(ed); break;
+        case ctrl('v'): paste_selection(ed); break;
+        case ctrl('s'): save(ed); break;
+        case ctrl('q'): if (quit(ed)) done = 1; break;
+        case ctrl('l'): redraw_screen(ed); break;
       }
     }
   }
@@ -1139,8 +1273,11 @@ int main(int argc, char *argv[]) {
   struct editor editbuf;
   struct editor *ed = &editbuf;
   int rc;
-  struct term *term;
   sigset_t blocked_sigmask, orig_sigmask;
+#ifdef __linux__
+  struct termios tio;
+  struct termios orig_tio;
+#endif
 
   if (argc != 2) {
     printf("usage: edit <filename>\n");
@@ -1157,9 +1294,15 @@ int main(int argc, char *argv[]) {
 
   setvbuf(stdout, NULL, 0, 8192);
 
-  term = gettib()->proc->term;
-  ed->cols = term->cols;
-  ed->lines = term->lines - 1;
+#ifdef __linux__
+  tcgetattr(0, &orig_tio);
+  cfmakeraw(&tio);  
+  tcsetattr(0, TCSANOW, &tio);
+  outstr("\033[3 q");  // xterm
+  outstr("\033]50;CursorShape=2\a");  // KDE
+#endif
+
+  get_console_size(ed);
   ed->linebuf = malloc(ed->cols + LINEBUF_EXTRA);
   
   sigemptyset(&blocked_sigmask);
@@ -1170,10 +1313,14 @@ int main(int argc, char *argv[]) {
 
   edit(ed);
 
+  gotoxy(0, ed->lines + 1);
+  outstr("\033[K");
+#ifdef __linux__
+  tcsetattr(0, TCSANOW, &orig_tio);   
+#endif
+
   delete_editor(ed);
 
-  gotoxy(0, term->lines);
-  outstr("\033[K");
   setbuf(stdout, NULL);
   sigprocmask(SIG_SETMASK, &orig_sigmask, NULL);
   return 0;
